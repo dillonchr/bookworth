83038ef93436ab4db9afd87f59fb56154091e804
Dillon Christensen <j.dillon.christensen@gmail.com>
2017-11-17 20:41:32 -0600

    refactored searcher to be singleton from the get-go

--- src/search/searcher.js
+++ src/search/searcher.js
@@ -3,39 +3,38 @@ const isAudiobook = s => /audiobook|[^\w]cd[^\w]|cds/i.test(s);
 const isLeather = s => /[^\w]leather|deluxe/i.test(s);
 const isLot = s => /[^\w]set[^\w]|lot of|[^\w]lot[^\w]/i.test(s);
 const isSigned = s => /signed|inscribed|autograph/i.test(s);
+const HideDuplicateItems = 'HideDuplicateItems';
+const SoldItemsOnly = 'SoldItemsOnly';
 const sellingStatuses = ['EndedWithSales', 'Active'];
 let jsonCallbackCounter = 0;
 
-class Searcher {
+export default {
     search(query) {
         return Promise.all([
             this.searchSoldListings(query),
             this.searchLiveListings(query)
         ])
-            .then(([sold, live]) => {
-                return sold.concat(live)
+            .then(results => {
+                return results
+                    .reduce((c, r) => c.concat(r), [])
                     .sort(function (a, b) {
                         return b.price - a.price;
                     });
             });
-    }
-
-    buildApiUrl({controller, filters = [], q, callback}) {
-        filters.unshift({
-            name: 'HideDuplicateItems',
-            value: 'true'
-        });
-        filters.map((f, i) => {
-            const v = `&itemFilter(${i + 1})`;
-            return `${v}.name=${f.name}${v}.value=${f.value}`;
-        });
-        return `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=${controller}&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${API_KEY}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=${callback}&REST-PAYLOAD${filters.join('')}&paginationInput.entriesPerPage=100&sortOrder=PricePlusShippingHighest&categoryId=267&keywords=${encodeURIComponent(q)}`;
-    }
-
-    apiFetch(options) {
+    },
+    buildApiUrl(controller, q, extraFilter, callback) {
+        const filterQuery = [HideDuplicateItems, extraFilter]
+            .filter(f => !!f)
+            .reduce((s, f, i) => {
+                const v = `&itemFilter(${i})`;
+                return `${s}${v}.name=${f}${v}.value=true`;
+            }, '');
+        return `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=${controller}&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${API_KEY}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=${callback}&REST-PAYLOAD${filterQuery}&paginationInput.entriesPerPage=100&sortOrder=PricePlusShippingHighest&categoryId=267&keywords=${encodeURIComponent(q)}`;
+    },
+    apiFetch(controller, q, extraFilter) {
         return new Promise((res, rej) => {
             const callback = `jsonp_${jsonCallbackCounter++}`;
-            const url = this.buildApiUrl({...options, callback});
+            const url = this.buildApiUrl(controller, q, extraFilter, callback);
             const script = document.createElement('script');
             script.onerror = rej;
             script.src = url;
@@ -48,9 +47,8 @@ class Searcher {
 
             document.head.appendChild(script);
         })
-            .then(listings => this.transformEbayResponse(listings));
-    }
-
+            .then(this.transformEbayResponse);
+    },
     transformEbayResponse(listings) {
         //  their JSON is all wrapped in single item arrays :/
         const [ firstKey ] = Object.keys(listings);
@@ -77,28 +75,11 @@ class Searcher {
                     sold: l.sellingStatus[0].sellingState[0] === sellingStatuses[0]
                 };
             });
-    }
-
+    },
     searchSoldListings(query) {
-        return this.apiFetch({
-            controller: 'findCompletedItems',
-            q: query,
-            filters: [
-                {
-                    name: 'SoldItemsOnly',
-                    value: 'true'
-                }
-            ]
-        });
-    }
-
+        return this.apiFetch('findCompletedItems', query, SoldItemsOnly);
+    },
     searchLiveListings(query) {
-        return this.apiFetch({
-            q: query,
-            controller: 'findItemsAdvanced'
-        });
+        return this.apiFetch('findItemsAdvanced', query);
     }
-}
-
-const searcher = new Searcher();
-export default searcher;
+};
