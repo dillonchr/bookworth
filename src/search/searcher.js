9dacb44797f32c6448d0aa80826bf524fcf76756
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-03-16 12:28:04 -0500

    fixed for ssl

--- src/search/searcher.js
+++ src/search/searcher.js
@@ -29,7 +29,7 @@ export default {
                 const v = `&itemFilter(${i})`;
                 return `${s}${v}.name=${f}${v}.value=true`;
             }, '');
-        return `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=${controller}&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${API_KEY}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=${callback}&REST-PAYLOAD${filterQuery}&paginationInput.entriesPerPage=100&sortOrder=PricePlusShippingHighest&categoryId=267&keywords=${encodeURIComponent(q)}`;
+        return `${window.location.protocol}//svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=${controller}&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${API_KEY}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=${callback}&REST-PAYLOAD${filterQuery}&paginationInput.entriesPerPage=100&sortOrder=PricePlusShippingHighest&categoryId=267&keywords=${encodeURIComponent(q)}`;
     },
     apiFetch(controller, q, extraFilter) {
         return new Promise((res, rej) => {
